6bf45efa8fcbb35f64789cbc52579a0f4af002bd
vanukuru.ravikiran <vanukuru.ravikiran@NA-JM67CY3-L.cnf.prod.cnf.com>
2024-05-21 22:45:08 -0700

    commit 1 demo3

---
+++ src/Test3/Demo3.java
@@ -0,0 +1,12 @@
+package Test3;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("print wow");
+		 System.out.println("print new line");
+		 System.out.println("print second new line");
+	}
+
+}
